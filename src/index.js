b944d1d75062cc14144309b775cda165f49db591
Cody <codywalenciak@gmail.com>
2019-08-16 20:29:22 -0500

    Stateful voting app, just figuring React out

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const libraryList = ['Angular', 'React', 'Vue', 'Ember', 'Meteor', 'Backbone', 'Handlebars']
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Panel(props){
+    return(
+        <div className="d-flex justify-content-center">
+        <div className="border border-info w-25 my-2">
+            
+            <div className="d-inline-block d-flex justify-content-center">
+            <h1 className="d-inline-block">{props.name}</h1>
+            </div>
+            <button className="btn btn-info btn-block"
+            onClick={()=>props.onClick()}
+            >
+            {props.votes}
+            </button>
+        </div>
+        </div>
+    )
+}
+
+class VotingBoard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            votes:Array(7).fill(0)
+        }
+    
+    }
+    renderPanel(i){
+        return(
+            <Panel
+            votes={this.state.votes[i]}
+            name={libraryList[i]}
+            onClick={()=>this.handleClick(i)}
+            />
+        )
+    }
+    handleClick(i){
+        const votes = this.state.votes.slice()
+        votes[i] = votes[i] + 1;
+        this.setState({
+            votes:votes
+        })
+    }
+    render(){
+        return(
+            <div className="votingBoard">
+                {this.renderPanel(0)}
+                {this.renderPanel(1)}
+                {this.renderPanel(2)}
+                {this.renderPanel(3)}
+                {this.renderPanel(4)}
+                {this.renderPanel(5)}
+                {this.renderPanel(6)}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <VotingBoard/>,
+    document.getElementById('root')
+)
